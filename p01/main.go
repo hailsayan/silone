8961f48b58d75ec7c5acb592ca08b1b071bb37c0
Psyon <sayan.h2020@gmail.com>
2024-02-25 13:00:22 +0330

    refactor 01

--- p01/main.go
+++ p01/main.go
@@ -1,20 +1,34 @@
 package main
 
-import "fmt"
+//34
 
-//24
+import (
+	"fmt"
+	"math"
+)
 
-func main() {
-	even()
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
-func even() {
-	for i := 1000; i <= 2000; i += 2 {
-		fmt.Println(i)
-		if i == 2000 {
-			break
-			fmt.Println("done")
-		}
+func main() {
+	var n int
+	fmt.Println("enter a number: ")
+	fmt.Scan(&n)
 
+	if isPrime(n) == true {
+		fmt.Printf("%d is a prime number\n", n)
+	} else {
+		fmt.Printf("%d is not a prime number\n", n)
 	}
+
 }
